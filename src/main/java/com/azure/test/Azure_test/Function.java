d4162db9e97d6b8b399b994b61a3bb2953e1c8ce
Rajat Kumar Padhan <rjtkumar29@gmail.com>
2023-05-22 22:09:21 +0530

    pom changes
    pom changes

--- src/main/java/com/azure/test/Azure_test/Function.java
+++ src/main/java/com/azure/test/Azure_test/Function.java
@@ -30,14 +30,6 @@ public class Function {
             final ExecutionContext context) {
         context.getLogger().info("Java HTTP trigger processed a request.");
 
-        // Parse query parameter
-        final String query = request.getQueryParameters().get("name");
-        final String name = request.getBody().orElse(query);
-
-        if (name == null) {
-            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
-        } else {
-            return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + name).build();
+            return request.createResponseBuilder(HttpStatus.OK).body("Hello....").build();
         }
-    }
 }
